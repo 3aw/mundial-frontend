0a247b00bea988026d705f28ea7c7ce8817df8ef
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-05 01:56:09 -0300

    adding router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import { GlobalStyle } from "./globals/globalStyle";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
-    </>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
