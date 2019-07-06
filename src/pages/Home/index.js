7679ecdb61c2a8ff93895fa8c6aa20a1bb102207
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-06 17:30:56 -0300

    testing slick

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 
 import { Container } from "./styles";
 import Header from "../../components/Header";
+import Carousel from "../../components/Carousel";
 
 export default class Home extends Component {
   render() {
     return (
       <Container>
         <Header />
+        <Carousel />
       </Container>
     );
   }
